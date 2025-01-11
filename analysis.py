622ce49d8c291b00f5ede373ca2097cdf79dd65c
zyj <edenzyj88@gmail.com>
2025-01-11 16:15:52 +0800

    refine code structure and add line chart drawing

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 import glob
 import json
+import matplotlib.pyplot as plt
 
 def modify_json(input_file_path, strings_to_check, output_file_path):
     """
@@ -44,41 +45,77 @@ def modify_json(input_file_path, strings_to_check, output_file_path):
     return modified_query_ids_with_num
 
 
-strings_to_check = []
-'''
-reference_dir = "reference_paper/"
-file_names = glob.glob(reference_dir + "*.txt")
-for file_name in file_names:
-    with open(file_name, 'r', encoding='utf-8') as fr:
-        content = fr.read()
-        paragraphs = content.split("\n")
+def draw_and_save_line_chart(data, output_file):
+    """
+    Draws a line chart using the given data and saves it as an image file.
 
-        for paragraph in paragraphs:
-            if len(paragraph) > 0:
-                strings_to_check.append(paragraph)
+    Parameters:
+    data (List[int]): Data of the y-axis.
+    output_file (str): The file name to save the figure.
+    """
+    # X-axis: Indices of the list
+    x = list(range(len(data)))
+    
+    # Create the plot
+    plt.figure(figsize=(10, 6))
+    plt.plot(x, data, marker='o', linestyle='-', color='b', label='Line Chart')
+    
+    # Set axis labels and title
+    plt.xlabel("Number of redundant chunks in retrieved context")
+    plt.ylabel("Number of queries")
+    plt.title("Number of Redundant Chunks in Retrieved Context within 1000 Queries")
+    
+    # Add grid and legend
+    plt.grid(True, linestyle='--', alpha=0.7)
+    plt.legend()
+    
+    # Save the figure
+    plt.savefig(output_file, format='png', dpi=300)
+    plt.close()
 
-        fr.close()
-'''
-redundant_file = "redundant_chunks/keywords.txt"
-with open(redundant_file, 'r') as fr:
-    content = fr.read()
-    paragraphs = content.split("\n")
-    for paragraph in paragraphs:
-        if len(paragraph) > 0:
-            strings_to_check.append(paragraph)
 
 input_file = "input_dir/9907_RR100_nFT_Llama32_1000Q_k10.json"
-output_file = "output_dir/9907_RR100_nFT_Llama32_1000Q_k10_noKeywords.json"
-analysis_file = "analysis/keywords.json"
+output_file = "output_dir/9907_RR100_nFT_Llama32_1000Q_k10_noAcknowledge.json"
+analysis_dir = "analysis/"
+analysis_file = analysis_dir + "Acknowledge.json"
+figure_file = analysis_dir + "Acknowledge.png"
 
 
 if __name__ == "__main__":
+    strings_to_check = []
+    '''
+    reference_dir = "reference_paper/"
+    file_names = glob.glob(reference_dir + "*.txt")
+    for file_name in file_names:
+        with open(file_name, 'r', encoding='utf-8') as fr:
+            content = fr.read()
+            paragraphs = content.split("\n")
+
+            for paragraph in paragraphs:
+                if len(paragraph) > 0:
+                    strings_to_check.append(paragraph)
+
+            fr.close()
+    '''
+    redundant_file = "redundant_chunks/acknowledge.txt"
+    with open(redundant_file, 'r', encoding="utf-8") as fr:
+        content = fr.read()
+        paragraphs = content.split("\n")
+        for paragraph in paragraphs:
+            if len(paragraph) > 0:
+                strings_to_check.append(paragraph)
+    
     modified_query_ids_with_num = modify_json(input_file, strings_to_check, output_file)
     
     print(modified_query_ids_with_num)
-    for num in modified_query_ids_with_num:
-        print(len(modified_query_ids_with_num[num]))
+    num_list = []
+    for chunk_num in modified_query_ids_with_num:
+        query_num = len(modified_query_ids_with_num[chunk_num])
+        num_list.append(query_num)
+        print(query_num)
         
     with open(analysis_file, 'w') as fw:
         json.dump(modified_query_ids_with_num, fw, indent=4)
         fw.close()
+    
+    draw_and_save_line_chart(num_list, figure_file)
